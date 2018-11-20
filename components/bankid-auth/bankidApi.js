bc4c813cc070154b5acd6c96f7ac53f8ef4b0d87
Eldin Behlic <eldinbehlic@gmail.com>
2018-11-20 08:42:15 +0100

    - minor changes

--- components/bankid-auth/bankidApi.js
+++ components/bankid-auth/bankidApi.js
@@ -2,16 +2,15 @@ const router = require('express').Router();
 const bankid = require('./bankid');
 
 router.get('/', async (req, res) => {
-    console.log("bankidapi")  
+    console.log('bankidapi');
     try {
         return res.json(
             await bankid.test()
         );
     } catch (err) {
-        console.log(err)
+        console.log(err);
         res.json(err);
     }
 });
 
 module.exports = router;
-
