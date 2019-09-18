b2444e168e9009c681736365369b41288deb4916
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-09-18 14:40:20 +0200

    Adds example for bookshelf.

--- src/db/db.bookshelf.js
+++ src/db/db.bookshelf.js
@@ -2,5 +2,29 @@ const { bookshelf } = require('./db.client');
 
 bookshelf.plugin('registry');
 
+/*  EXAMPLES 
+    Implement your db models. 
+    @docs: https://bookshelfjs.org/
+
+    const Questions = bookshelf.Model.extend({
+        tableName: 'questions',
+        form() { return this.belongsTo(Forms); },
+        validations() { return this.hasMany(Validations); },
+        optionGroup() { return this.hasOne(OptionGroups); },
+        choices() { return this.belongsToMany(OptionChoices); },
+        type() { return this.hasOne(QuestionTypes); },
+    });
+    
+    const QuestionTypes = bookshelf.Model.extend({
+        tableName: 'question_types',
+        questions() { return this.belongsToMany(Questions); },
+    });
+
+    module.exports = {
+        questions: bookshelf.model('Questions', Questions),
+        questionTypes: bookshelf.model('QuestionTypes', QuestionTypes),
+    };
+*/ 
+
 module.exports = {
 };
