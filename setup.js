f093791124b929a15da16076403dce8a8850c5a6
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-09-19 09:47:13 +0200

    Fix linting errors

--- setup.js
+++ setup.js
@@ -1,22 +1,20 @@
 const fs = require('fs');
 
-//Defines copy targets
-const sourceFile = '.env-example'; 
+const sourceFile = '.env-example';
 const createFiles = ['.env', '.env.test'];
 
-//Create files 
-createFiles.forEach(function(createFile) {
-    try {
-        if(!fs.existsSync(createFile)) {
-            fs.createReadStream(sourceFile).pipe(fs.createWriteStream(createFile));
-            console.log("Created file " + createFile + " from " + sourceFile + "."); 
-        } else {
-            console.error("File " + createFile + " already exists."); 
-        }
-    } catch(err) {
-        console.error(err)
+createFiles.forEach((createFile) => {
+  try {
+    if (!fs.existsSync(createFile)) {
+      fs.createReadStream(sourceFile).pipe(fs.createWriteStream(createFile));
+      console.log('Created file ${createFile} from sourceFile');
+    } else {
+      console.error('File ${createFile} already exists.');
     }
-}); 
+  } catch (err) {
+    console.error(err);
+  }
+});
 
-//Done
-console.log("Setup completed. Additional log information above."); 
+// Done
+console.log('Setup completed. Additional log information above.');
