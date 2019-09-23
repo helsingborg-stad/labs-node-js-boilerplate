e1559bbe6370ff52f01c55102ebe7991d467843f
Teddy Andersson <teddyandersson@Teddys-MBP.brabit.se>
2019-09-23 13:57:21 +0200

    Swagger JsonApi documentation
    The boilerplates swagger documenation is now updated to support swagger with definitions based on jsonapi.org standard.

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const https = require('https');
 const pino = require('express-pino-logger');
 const swaggerUi = require('swagger-ui-express');
 const bodyParser = require('body-parser');
+const jsonSchemaRefParser = require('json-schema-ref-parser');
 const swaggerDocument = require('../swagger/swagger.js');
 const routes = require('./components/routes');
 const logger = require('./utils/logger');
@@ -24,31 +25,47 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Request logging
+/**
+ * Logging
+ */
 app.use(pino({ logger }));
 
-// Add routes to the app.
+/**
+ * Routes
+ */
 app.get('/', (req, res) => res.send('Hello World!'));
 app.use(API_BASE, routes());
 
-// Swagger for documenting the api, access through localhost:xxxx/api-docs.
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+/**
+ * Swagger
+ * Access documentation through localhost:xxxx/api-docs.
+ */
+
+jsonSchemaRefParser.dereference(swaggerDocument, (err, schema) => {
+  if (err) {
+    console.error(err);
+  } else {
+    // `schema` is just a normal JavaScript object that contains your entire JSON Schema,
+    // including referenced files, combined into a single object
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(schema));
+  }
+});
 
 const server = https.createServer(app);
 
 /**
  * Create WebSocket server
  */
-//const webSocketServer = new WebSocketServer(server, `${API_BASE}/ws`);
+// const webSocketServer = new WebSocketServer(server, `${API_BASE}/ws`);
 
 /**
  * Start
+ * Listen on port specfied in env-file.
  */
 
-// Listen on port specfied in env-file.
 server.listen({ port: PORT }, async () => {
   logger.info(`Server started on port ${PORT}`);
-  //webSocketServer.start();
+  // webSocketServer.start();
 });
 
 // Export server to use it in tests.
