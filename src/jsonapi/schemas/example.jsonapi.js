cb47df1b4aef64876ae0e2983457a77e66e5f4fc
Teddy Andersson <teddyandersson@Teddys-MacBook-Pro.local>
2019-09-25 11:30:09 +0200

    Update example jsaonapi schema

--- src/jsonapi/schemas/example.jsonapi.js
+++ src/jsonapi/schemas/example.jsonapi.js
@@ -7,31 +7,14 @@ const { BASE_URL } = process.env;
 
 const formJsonApiSchema = {
   id: 'id',
-  blacklist: [], // blacklist keys that you don't want to show in the reponse object.
+  blacklist: ['userId'], // blacklist keys that you don't want to show in the reponse object.
   links: {
     self(data) {
-      return `${BASE_URL}/example/${data.id}`; //
+      return `${BASE_URL}/examples/${data.id}`; //
     },
-  },
-  relationships: {
-    questions: {
-      type: 'example',
-      links(data) {
-        return {
-          self: `${BASE_URL}/example/${data.id}`,
-          related: `${BASE_URL}/example/${data.id}`,
-        };
-      },
-    },
-  },
-  topLevelMeta(data, extraData) {
-    return {
-      count: extraData.count,
-      total: data.length,
-    };
   },
   topLevelLinks: {
-    self: '/example/',
+    self: `${BASE_URL}/examples`,
   },
 };
 
