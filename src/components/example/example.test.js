8c96157726fc3d1f1f48ff1faf1a4b612089b5fa
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-10-03 14:56:43 +0200

    Working unit tests

--- src/components/example/example.test.js
+++ src/components/example/example.test.js
@@ -3,26 +3,27 @@ require('dotenv').config({ path: './.env.test' });
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../../server');
-const Persons = require('./person.db');
+// const Example = require('./example.db');
 const { client } = require('../../db/db.client');
 
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('Person', () => {
+describe('Example', () => {
+  
   after(async () => {
     server.close();
-    await Persons.reset();
+    // await Example.reset();
     await client.destroy();
   });
 
   beforeEach(async () => {
-    await Persons.reset();
+    // await Example.reset();
   });
 
-  it('should return json on GET with faked query', async () => chai
+  it('should return json on GET with example query', async () => chai
     .request(server)
-    .get('/person/fake')
+    .get('/api/v1/examples')
     .send()
     .then((res) => {
       res.should.have.status(200);
@@ -30,9 +31,9 @@ describe('Person', () => {
       should.exist(res.body);
     }));
 
-  it('should return json on GET with correct query params at fake', async () => chai
+    it('should return json on GET with example query on a id', async () => chai
     .request(server)
-    .get('/person/fake?person_id=john_snow')
+    .get('/api/v1/examples/:id')
     .send()
     .then((res) => {
       res.should.have.status(200);
@@ -40,70 +41,4 @@ describe('Person', () => {
       should.exist(res.body);
     }));
 
-  it('should allow POST with valid body at fake', async () => chai
-    .request(server)
-    .post('/person/fake')
-    .send({
-      person_id: 'john_snow',
-    })
-    .then((res) => {
-      res.should.have.status(200);
-      res.should.be.json;
-      should.exist(res.body);
-    }));
-
-
-  it('should return json on GET at fake', async () => chai
-    .request(server)
-    .get('/person/fake')
-    .send()
-    .then((res) => {
-      res.should.have.status(200);
-      res.should.be.json;
-      should.exist(res.body);
-    }));
-
-  it('should return json on GET with correct query params', async () => chai
-    .request(server)
-    .get('/person?person_id=john_snow')
-    .send()
-    .then((res) => {
-      res.should.have.status(200);
-      res.should.be.json;
-      should.exist(res.body);
-    }));
-
-  it('should allow POST with valid body on real query', async () => chai
-    .request(server)
-    .post('/person')
-    .send({
-      person_id: 'john_snow',
-    })
-    .then((res) => {
-      res.should.have.status(200);
-      should.exist(res.body);
-    }));
-
-  it('should correctly add entity to db on POST and return the new entity on GET', async () => {
-    const requester = chai.request(server).keepOpen();
-
-    await requester
-      .post('/person')
-      .send({
-        person_id: 'john_snow',
-      });
-
-    await requester
-      .get('/person?person_id=john_snow')
-      .send()
-      .then((res) => {
-        res.should.have.status(200);
-        res.should.be.json;
-        should.exist(res.body);
-        res.body[0].should.have.property('person_id');
-        res.body[0].person_id.should.equal('john_snow');
-      });
-
-    requester.close();
-  });
 });
